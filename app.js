5265f6d06563edff4a2bf399b8f9982c49b7d045
Devan Cobb <devancobb.dev@gmail.com>
2022-01-23 18:12:45 -0700

    psuedocode for js file thru startGame() function

---
+++ app.js
@@ -0,0 +1,15 @@
+// 1. Create two variables, firstCard and secondCard.
+// 2.  Set their values to a random number between 2-11
+// 3. Create a variable, sum, and set it to the sum of the two cards
+// 4. Write the conditional according to these rules:
+//  if less than or equal to 20 -> "Do you want to draw a new card?"
+//  else if exactly 21 -> "You've got Blackjack!"
+//  else -> "You're out of the game!"
+// 5. Create a variable called hasBlackJack and assign it to false
+// 6. Create a variable called isAlive and assign it to true
+// 7. Flip its value to false in the appropriate code block
+// 8. Declare a variable called message and assign its value to an empty string
+// 9. Reassign the message variable to the string thats logged out
+// 10. Create a startGame() function. Move the conditional
+//  below (line 11-20) inside the body of the function.
+// 11.
