b92c5e43106b44c94ad435377c91b2389d4d0df8
Devan Cobb <devancobb.dev@gmail.com>
2022-01-23 18:22:56 -0700

    translated psuedo to code in app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // 1. Create two variables, firstCard and secondCard.
-// 2.  Set their values to a random number between 2-11
+// 2. Set their values to a random number between 2-11
 // 3. Create a variable, sum, and set it to the sum of the two cards
 // 4. Write the conditional according to these rules:
 //  if less than or equal to 20 -> "Do you want to draw a new card?"
@@ -12,4 +12,29 @@
 // 9. Reassign the message variable to the string thats logged out
 // 10. Create a startGame() function. Move the conditional
 //  below (line 11-20) inside the body of the function.
-// 11.
+// 11. Store the message-el paragraph in a variable called messageEl
+// 12. Display the message in the messageEl
+
+
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+let messageEl = document.getElementById("message-el")
+console.log(messageEl)
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
